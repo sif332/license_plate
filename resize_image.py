60efee40f3c5b9c94167ed145e296028d33c2c4c
Settawut Kaewtima <sif332@gmail.com>
2022-11-08 14:04:46 +0700

    Create resize_image.py

---
+++ resize_image.py
@@ -0,0 +1,40 @@
+#resize image keeping aspect ratio
+import cv2
+from PIL import Image
+def image_resize(image, width = None, height = None, inter = cv2.INTER_AREA):
+    # initialize the dimensions of the image to be resized and
+    # grab the image size
+    dim = None
+    (h, w) = image.shape[:2]
+
+    # if both the width and height are None, then return the
+    # original image
+    if width is None and height is None:
+        return image
+
+    # check to see if the width is None
+    if width is None:
+        # calculate the ratio of the height and construct the
+        # dimensions
+        r = height / float(h)
+        dim = (int(w * r), height)
+
+    # otherwise, the height is None
+    else:
+        # calculate the ratio of the width and construct the
+        # dimensions
+        r = width / float(w)
+        dim = (width, int(h * r))
+
+    # resize the image
+    resized = cv2.resize(image, dim, interpolation = inter)
+
+    # return the resized image
+    return resized
+
+img = "/home/xai/yolov5/input_image/300x180.jpg"
+img = cv2.imread(img)
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+img = image_resize(img, height = 800)
+img = Image.fromarray(img)
+img.save("test1.jpg")
